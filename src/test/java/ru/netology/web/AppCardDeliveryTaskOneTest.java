5b144812b442a30498a1c3a499d65846fbdce06e
TriksterTripster <1387751@mail.ru>
2023-03-14 22:01:13 +0500

    fix tests

---
+++ src/test/java/ru/netology/web/AppCardDeliveryTaskOneTest.java
@@ -0,0 +1,41 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AppCardDeliveryTaskOneTest {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldBeSuccessfullyCompleted() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Самара");
+        String currentDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        $("[data-test-id='name'] input").setValue("Иванов-Петрович Иван");
+        $("[data-test-id='phone'] input").setValue("+79370645456");
+        $("[data-test-id='agreement']").click();
+        $(".button.button").click();
+        $(".notification_content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+
+    }
+}
+
+
+
+
